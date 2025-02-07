85a01dbbabd5cc2954ec639c49f612c864018fe6
diego forero cruz <df.foreroc@uniandes.edu.co>
2025-02-06 20:47:00 -0500

    se realiza la implementacion parcial del form

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker_app/models/categories.dart';
 import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -29,7 +30,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAdd() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => Text('modal sheet'),
+      builder: (ctx) => NewExpense(),
     );
   }
 
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return NewExpenseState();
+  }
+}
+
+class NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            decoration: InputDecoration(
+              label: Text('Title'),
+            ),
+            controller: _titleController,
+          ),
+          Row(
+            children: [
+              TextField(
+                maxLength: 20,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  prefixText: '\$ ',
+                  label: Text('Amount'),
+                ),
+                controller: _amountController,
+              ),
+              SizedBox()
+            ],
+          ),
+          Row(
+            children: [
+              Text("data"),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: Text('Save Expense'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
